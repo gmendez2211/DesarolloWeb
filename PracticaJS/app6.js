4e0ce0b577a183872b60b6243aafe71c549c64cc
gmendez <gmendez.2211@gmail.com>
2024-07-26 21:49:07 -0600

    Ejemplo de objetos clases y arreglos

--- PracticaJS/app6.js
+++ PracticaJS/app6.js
@@ -1,12 +1,23 @@
 /**
- * Funciiones
+ * Objetos
  * 
  */
 
-let Num1 = 10;
-let Num2 = 20;
-function fSumar(Num1, Num2){
-  return Num1 + Num2
-}
+var Persona = new Object();
+Persona.sNombre = "Pedro";
+Persona.sApellido = "Picapiedra";
+Persona.nEdad = 100;
 
-console.log('La suma es: ' + fSumar(Num1, Num2))
+console.log('La persona es: ' + Persona.sNombre );
+
+
+//Objetos anidados
+const MiCarro = {
+    color: "red",
+    reudas: 4,
+    motor: { cilindro: 4, tamanio: 2.2 },
+  };
+  //Imprime Object
+  console.log('El motor de mi carro es: ' + MiCarro.motor );
+  //Imprime number
+  console.log('El motor de mi carro es: ' + typeof MiCarro.motor.cilindro );
