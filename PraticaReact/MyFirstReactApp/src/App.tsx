e2187ce3a3786caf398546ec30c6cadc87f7e346
gmendez <gmendez.2211@gmail.com>
2024-08-11 05:18:23 -0600

    Componentes

--- PraticaReact/MyFirstReactApp/src/App.tsx
+++ PraticaReact/MyFirstReactApp/src/App.tsx
@@ -1,8 +1,20 @@
-import Title from "./title";
+//import Card from "./components/Card";
+import Card3 from "./components/Card3";
 import "./App.css";
+import List from "./components/List";
 
 function App() {
-  return <Title />;
+  //Arreglo para trasladar a la lista
+  const lList = ["Monitor", "Teclado", "Mouuse"];
+
+  //return <Card />;
+  // return <Card2 body={"Hola mundo"} />;
+  return (
+    <Card3>
+      Hola Mundo
+      <List data={lList} />
+    </Card3>
+  );
 }
 
 export default App;
