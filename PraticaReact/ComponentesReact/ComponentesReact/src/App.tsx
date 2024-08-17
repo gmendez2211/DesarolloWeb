b127019ddfb37699a936eff1293ed57ef4650c5b
gmendez <gmendez.2211@gmail.com>
2024-08-17 04:00:08 -0600

    Servicio

--- PraticaReact/ComponentesReact/ComponentesReact/src/App.tsx
+++ PraticaReact/ComponentesReact/ComponentesReact/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 //import Button from "./componentes/Button";
-import Form from "./componentes/Form";
+//import Form from "./componentes/Form";
+import ConsultaJSON from "./view/ConsultaJSON";
 
 function App() {
-  return <Form />;
+  return <ConsultaJSON />;
+  //<Form />;
   //(<Button texto="Mi Prime Boton" />);
 }
 
