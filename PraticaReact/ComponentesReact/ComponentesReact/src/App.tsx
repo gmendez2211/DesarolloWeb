ee5239772763814cb78f517277d228afae07a57a
gmendez <gmendez.2211@gmail.com>
2024-08-16 23:37:26 -0600

    Ejemplo de componentes

---
+++ PraticaReact/ComponentesReact/ComponentesReact/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+//import Button from "./componentes/Button";
+import Form from "./componentes/Form";
+
+function App() {
+  return <Form />;
+  //(<Button texto="Mi Prime Boton" />);
+}
+
+export default App;
