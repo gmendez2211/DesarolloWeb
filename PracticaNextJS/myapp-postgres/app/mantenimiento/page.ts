54f93795f18c6e722855cefac20a993765412301
gmendez <gmendez.2211@gmail.com>
2024-09-21 05:30:41 -0600

    Se modifica metodo POST

--- PracticaNextJS/myapp-postgres/app/mantenimiento/page.ts
+++ PracticaNextJS/myapp-postgres/app/mantenimiento/page.ts
@@ -2,18 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { conn } from "@/app/utils/database";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { name, email } = req.body;
+  const { method, body } = req;
+  switch (method) {
+    case "POST":
+      try {
+        const { name, email } = body;
+        const query =
+          "INSERT INTO tbl_usuarios (name, email) VALUES ($1, $2) RETURNING *";
+        const values = [name, email];
+        const result = await conn.query(query, values);
 
-  try {
-    const query =
-      "INSERT INTO tbl_usuarios (name, email) VALUES ('emendez', 'emendez@micorreo.com') RETURNING *";
-    const values = [name, email];
-    const result = await conn.query(query, values);
-
-    res.status(200).json(result.rows[0]);
-  } catch (error) {
-    console.error("Error inserting user:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+        return res.status(200).json(result.rows[0]);
+      } catch (error) {
+        console.error("Error inserting user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
   }
 };
