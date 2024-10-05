52f9d6c7a005ef54237aecbe8a679b9a9a6f585f
gmendez <gmendez.2211@gmail.com>
2024-10-05 06:32:14 -0600

    Se agrega return de correcto

--- PracticaNextJS/app-fullstack/app/api/consultas/[id]/route.ts
+++ PracticaNextJS/app-fullstack/app/api/consultas/[id]/route.ts
@@ -6,23 +6,30 @@ export const GET = async (
   { params }: { params: { id: number } }
 ) => {
   const _Id = params.id;
-
+  console.log("Id ", _Id);
   try {
     const query =
-      "SELECT primernombre, segundonombre, primerapellido, segundoapellido FROM tbl_usuarios WHERE id = $1 RETURNING *";
+      "SELECT primernombre, segundonombre, primerapellido, segundoapellido FROM tbl_usuarios WHERE id = $1";
 
     //console.log("Valores: ", values, " Metodo: ", method);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const result = await conn.query(query, [_Id]);
-
-    return new NextResponse(JSON.stringify("Usuario: " + result.rows[0]), {
-      status: 200,
-    });
+    /*
+    return new NextResponse(
+      JSON.stringify("Usuario:" + result.rows.toString()),
+      {
+        status: 200,
+        headers: { "content-type": "application/json" },
+      }
+    );
+    */
+    return Response.json(result.rows[0]);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    console.error("Error inserting user:", error);
+    console.error("Error consultando usuario:", error);
     return new NextResponse("Error interno del servidor " + error.message, {
       status: 500,
+      headers: { "content-type": "application/json" },
     });
   }
 };
